d9cf6a97d8b49c4e7de858d5d2c5a2c8661a4d5d
Markus Guidry <markus@gu1dry.com>
2014-03-09 01:37:33 -0500

    n5100: Align ro.product with stock
    Additional general cleanup.
    Set fingerprint to a valid fingerprint N5100XXCMK1. Previously
    listed one was not completely valid.
    
      PS2: Actually set the proper settings, derp...
    
    Change-Id: I9be1cd9161f884673a583714b791f8db7efef001

--- omni_n5100.mk
+++ omni_n5100.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2013 OmniROM Project
 # Copyright (C) 2012 The CyanogenMod Project
 #
@@ -12,13 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-
-# Specify phone tech before including full_phone
+# Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := n5100
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -26,12 +24,19 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/n5100/full_n5100.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := n5100
+
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := n5100
-PRODUCT_NAME := omni_n5100
-PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N5100
+PRODUCT_BRAND := samsung
+PRODUCT_NAME := omni_n5100
+PRODUCT_DEVICE := n5100
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-N5100 TARGET_DEVICE=GT-N5100 BUILD_FINGERPRINT="samsung/kona3gxx/kona3g:4.3/JSS15J/N5100XXBMD1:user/release-keys" PRIVATE_BUILD_DESC="kona3gxx-user 4.3 JSS15J N5100XXBMD1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=kona3gxx \
+    TARGET_DEVICE=kona3g \
+    PRIVATE_BUILD_DESC="kona3gxx-user 4.2.2 JDQ39 N5100XXCMK1 release-keys" \
+    BUILD_FINGERPRINT="samsung/kona3gxx/kona3g:4.2.2/JDQ39/N5100XXCMK1:user/release-keys"
